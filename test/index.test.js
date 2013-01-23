8dff40e4f27395d2d18b40d3cf57560a70995c06
Jackson Tian <shyvo1987@gmail.com>
2013-01-23 16:10:28 +0800

    Add test cases

---
+++ test/index.test.js
@@ -0,0 +1,28 @@
+var api = require('../');
+
+describe('index', function () {
+  describe('search', function () {
+    it('should ok', function () {
+      var result = api.search('module');
+      result.status.should.be.equal('MATCHED');
+      result.result.should.have.length(4);
+      result.should.have.property('more');
+      result.more.should.have.length(2);
+      result.result.forEach(function (item) {
+        item.should.have.property('textRaw');
+        item.should.have.property('name', 'module');
+        item.should.have.property('type');
+        item.should.have.property('desc');
+        item.should.have.property('path');
+      });
+    });
+
+    it('should ok', function () {
+      var result = api.search('hehe');
+      result.status.should.be.equal('UNMATCHED');
+      result.result.should.have.length(0);
+      result.should.not.have.property('more');
+    });
+  });
+});
+
